02c0b16873423af6a25984646bd794a1d52ea1e0
Joakim Saario <saario.joakim@gmail.com>
2020-03-03 00:20:03 +0100

    Initial commit

---
+++ drum_machine/timeline.py
@@ -0,0 +1,28 @@
+import itertools
+from collections import defaultdict
+
+
+class Timeline:
+    def __init__(self, bpm):
+        self.bpm = bpm
+        self._timeline = defaultdict(list)
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            if item.step is not None:
+                raise NotImplementedError(
+                    f'slice.step for {__name__}.{self.__class__.__name__} '
+                    'is not not implemented.'
+                )
+
+            start, stop = item.start or 0, item.stop
+            iterator = itertools.count() if stop is None else range(start, stop)
+            return (self._timeline[start + delta] for delta in iterator)
+        else:
+            return self._timeline[item]
+
+    def __iter__(self):
+        yield self[:]
+
+    def add_sample(self, beat, sample):
+        self._timeline[beat] += [sample]
